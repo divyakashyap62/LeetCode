47680d599abf9559a8cbd9254023623225be4c24
divyakashyap62 <divyakashyap626@gmail.com>
2026-08-10 10:23:22 +0530

    Time: 3 ms (85.89%), Space: 71.9 MB (44.36%) - LeetHub

---
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int count = 0;
+        int currentSum = 0;
+        int targetSum = k * threshold;
+        
+        
+        for (int i = 0; i < k; i++) {
+            currentSum += arr[i];
+        }
+        
+        if (currentSum >= targetSum) {
+            count++;
+        }
+        
+        
+        for (int i = k; i < arr.length; i++) {
+            currentSum += arr[i] - arr[i - k];
+            if (currentSum >= targetSum) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
