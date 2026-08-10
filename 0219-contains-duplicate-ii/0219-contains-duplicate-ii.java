7232dc0741afd1c4bf85eb9e553a97d0763f67a8
divyakashyap62 <divyakashyap626@gmail.com>
2026-08-10 10:06:17 +0530

    Time: 29 ms (26.73%), Space: 91.8 MB (43.36%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,29 @@
+
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+        if (nums == null || nums.length <= 1 || k <= 0) {
+            return false;
+        }
+        
+        HashSet<Integer> window = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (window.contains(nums[i])) {
+                return true;
+            }
+            
+            
+            window.add(nums[i]);
+            
+            
+            if (window.size() > k) {
+                window.remove(nums[i - k]);
+            }
+        }
+        
+        return false;
+    }
+}
